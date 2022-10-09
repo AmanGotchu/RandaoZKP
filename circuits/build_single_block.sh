42600b95a6dfc029cb923edfc225ac65f1737273
dlee <dlee@paradigm.xyz>
2022-10-09 03:49:37 -0500

    move verification key references to trusted_setup folder

--- circuits/build_single_block.sh
+++ circuits/build_single_block.sh
@@ -2,6 +2,7 @@
 
 PHASE1=/powers-of-tau/powersOfTau28_hez_final_25.ptau
 BUILD_DIR=$BUILD_DIR
+TRUSTED_SETUP_DIR=../trusted_setup/
 CIRCUIT_NAME=singleBlockHeader
 
 if [ ! -d "$BUILD_DIR" ]; then
@@ -33,43 +34,43 @@ else
 fi
 
 # these need powers of tau...
-if test ! -f "$BUILD_DIR/vkey.json"; then
+if test ! -f "$TRUSTED_SETUP_DIR/vkey.json"; then
     echo "****GENERATING ZKEY 0****"
     start=`date +%s`
-    NODE_OPTIONS="--max-old-space-size=56000" npx snarkjs groth16 setup "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey
+    NODE_OPTIONS="--max-old-space-size=56000" npx snarkjs groth16 setup "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$TRUSTED_SETUP_DIR"/"$CIRCUIT_NAME"_0.zkey
     end=`date +%s`
     echo "DONE ($((end-start))s)"
 
     echo "****GENERATING FINAL ZKEY****"
     start=`date +%s`
-    NODE_OPTIONS="--max-old-space-size=56000" npx snarkjs zkey beacon "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey "$BUILD_DIR"/"$CIRCUIT_NAME".zkey 0102030405060708090a0b0c0d0e0f101112231415161718221a1b1c1d1e1f 10 -n="Final Beacon phase2"
+    NODE_OPTIONS="--max-old-space-size=56000" npx snarkjs zkey beacon "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey "$TRUSTED_SETUP_DIR"/"$CIRCUIT_NAME".zkey 0102030405060708090a0b0c0d0e0f101112231415161718221a1b1c1d1e1f 10 -n="Final Beacon phase2"
     end=`date +%s`
     echo "DONE ($((end-start))s)"
 
     echo "****VERIFYING FINAL ZKEY****"
     start=`date +%s`
-    NODE_OPTIONS="--max-old-space-size=56000" npx snarkjs zkey verify -verbose "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME".zkey
+    NODE_OPTIONS="--max-old-space-size=56000" npx snarkjs zkey verify -verbose "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$TRUSTED_SETUP_DIR"/"$CIRCUIT_NAME".zkey
     end=`date +%s`
     echo "DONE ($((end-start))s)"
 
     echo "****EXPORTING VKEY****"
     start=`date +%s`
-    npx snarkjs zkey export verificationkey "$BUILD_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/vkey.json
+    npx snarkjs zkey export verificationkey "$TRUSTED_SETUP_DIR"/"$CIRCUIT_NAME".zkey "$TRUSTED_SETUP_DIR"/vkey.json
     end=`date +%s`
     echo "DONE ($((end-start))s)"
 fi
 
 echo "****GENERATING PROOF FOR SAMPLE INPUT****"
 start=`date +%s`
-npx snarkjs groth16 prove "$BUILD_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/witness.wtns "$BUILD_DIR"/proof.json "$BUILD_DIR"/public.json
+npx snarkjs groth16 prove "$TRUSTED_SETUP_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/witness.wtns "$BUILD_DIR"/proof.json "$BUILD_DIR"/public.json
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****VERIFYING PROOF FOR SAMPLE INPUT****"
 start=`date +%s`
-npx snarkjs groth16 verify "$BUILD_DIR"/vkey.json "$BUILD_DIR"/public.json "$BUILD_DIR"/proof.json
+npx snarkjs groth16 verify "$TRUSTED_SETUP_DIR"/vkey.json "$BUILD_DIR"/public.json "$BUILD_DIR"/proof.json
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 # generate the solidity verifier contract
-snarkjs zkey export solidityverifier "$BUILD_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_verifier.sol
+snarkjs zkey export solidityverifier "$TRUSTED_SETUP_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_verifier.sol
